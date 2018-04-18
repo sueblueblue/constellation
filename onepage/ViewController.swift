ce96de67636c9ae239e8151c8bc3ef0cb3d0169c
CCAR <ccar@ccar-de-macbook-air.local>
2018-04-18 19:03:03 +0800

    Initial Commit

---
+++ onepage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  onepage
+//
+//  Created by CCAR on 2018/4/18.
+//  Copyright © 2018年 CCAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
